79e1746b131a27259c6a361e1e24174313efd23d
Xavier Wang <duley666@gmail.com>
2019-05-19 22:52:42 +0800

    correct typo, set schedule

--- app/Commands/Notifier/LeadersCommand.php
+++ app/Commands/Notifier/LeadersCommand.php
@@ -29,19 +29,19 @@ class LeadersCommand extends Command
      */
     public function handle()
     {
-        $meetingTimestame = strtotime('next tuesday');
+        $meetingTimestamp = strtotime('next tuesday');
 
         $google_client = $this->getGoogleClient();
         $service = new \Google_Service_Drive($google_client);
         $postBody = new \Google_Service_Drive_DriveFile([
-            'name' => date('Y-m-d', $meetingTimestame),
+            'name' => date('Y-m-d', $meetingTimestamp),
         ]);
         $newFile = $service->files->copy(env('DOC_TEMPLATE_ID'), $postBody);
         $this->info(sprintf("file copied. Name: %s", $newFile->name));
 
         $msg = sprintf(
             "<!here|here> 本週 %s（二）21:00 - 22:00 會議記錄，如有工作進度及需議決事項請至上面填寫哦~\n<https://docs.google.com/document/d/%s/edit>",
-            date('m-d', $meetingTimestame),
+            date('m-d', $meetingTimestamp),
             $newFile->id
         );
         $json = [
@@ -67,12 +67,12 @@ class LeadersCommand extends Command
      */
     public function schedule(Schedule $schedule): void
     {
-        // $schedule->command(static::class)->everyMinute();
+        $schedule->command(static::class)->weeklyOn(0, '0:00');
     }
 
     protected function getGoogleClient()
     {
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path() . env('SECRET_FOLDER') . env('GOOGLE_SERVICE_CRENDENTIALS'));
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path() . env('SECRET_FOLDER') . env('GOOGLE_SERVICE_CREDENTIALS'));
 
         $client = new \Google_Client();
         $client->setApplicationName(env('GOOGLE_APP_NAME'));
