75c6df60a51a4281865df2f17265fd1d35881089
Xavier Wang <duley666@gmail.com>
2019-05-21 15:10:52 +0800

    send message to slack channel: info-team

---
+++ app/Commands/Notifier/InfoTeamCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Commands\Notifier;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use GuzzleHttp\Client;
+
+class InfoTeamCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:infoteam';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'notify info team to report sprint progress.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $msg = <<<EOT
+<!channel>
+Hi All, 提醒大家每週五所有成員要回報目前進度喔，格式如下：
+組別： xxxx
+這週做了：
+    1. xxxxx
+    2. xxxxx
+下週要做：
+    1. xxxxx
+    2. xxxxx
+遇到什麼問題：
+    1. xxxxx
+    2. xxxxx
+EOT;
+
+        $response = (new Client())->request(
+            'POST',
+            env('SLACK_INFOTEAM_WEBHOOK'),
+            ['json' => ['text' => $msg]]
+        );
+
+        if ($response->getBody() == 'ok') {
+            $this->info('slack message sent.');
+        }
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        $schedule->command(static::class)->weeklyOn(0, '9:00');
+    }
+}
