0e0a45658f683cb816d0acbf5560d4d132904295
root <root@b0ec0ae2fe23>
2021-09-07 16:42:18 +0800

    更新講者資料轉換格式

--- pure_php/convert_speaker.php
+++ pure_php/convert_speaker.php
@@ -1,14 +1,45 @@
 <?php
+
 /**
  * $ php convert_speaker.php speakers.tsv speaker.json
  */
+
+$tagTrans = [
+    'ai'                     => 'AI',
+    'arvr'                   => 'AR/VR',
+    'blockchain'             => 'Blockchain',
+    'cloud'                  => 'Cloud',
+    'devops'                 => 'DevOps',
+    'iot'                    => 'IoT',
+    'mobile_app'             => 'Mobile App',
+    'startup'                => 'Startup',
+    'uiux'                   => 'UI/UX',
+    'web'                    => 'Web',
+    'quant'                  => 'Quant',
+    'security'               => 'Security',
+    'cross_platform'         => 'Cross-platform',
+    'data_science'           => 'Data Science',
+    'agile'                  => 'Agile',
+    'panel'                  => 'Panel',
+    'fin_tech'               => 'FinTech',
+    'qa'                     => 'QA',
+    'data_analyzing'         => 'Data Analyzing',
+    '5g6g'                   => '5G / 6G',
+    'business_thinking'      => 'Business Thinking',
+    'social_engagement'      => '社會參與',
+    'career_development'     => '職涯發展',
+    'digital_transformation' => '數位轉型',
+    'remote_work'            => '遠距',
+    'community'              => 'Community',
+    'open_source'            => 'Open Source',
+];
 
 $extraField = [
     'started_at' => 0,
     'ended_at' => 0,
     'room' => '',
     'floor' => '',
-    'sponsor_id' => 0,
+    'sponsor' => [],
 ];
 
 $file = $argv[1];
@@ -33,26 +64,38 @@ while (($row = fgets($f)) !== false) {
         $firstLine = false;
         continue;
     }
-
     $row = trim($row);
     $result = explode("\t", $row);
     $speaker_id = (int) $result[0];
 
-    if (!in_array(trim($result[34]), ['關閉前台修改', '已確認'])) {
+    if (!in_array(trim($result[43]), ['關閉前台修改', '已確認'])) {
         continue;
     }
 
-
     array_walk_recursive($result, function (&$value) {
-        $value = str_replace(array('\\x22','\\x27','\\n'), array("'",'"',"\n"), $value);
+        $value = str_replace(array('\\x22', '\\x27', '\\n'), array("'", '"', "\n"), $value);
     });
-    
+
     echo $result[1];
+
     echo "\n";
-    //tag filter empty
-    $result[23] = $result[23] == '' ? [] : explode(',', $result[23]);
-    //convert keynote
-    $result[37] = $result[37] === '是';
+    // tag filter empty
+    $result[28] = $result[28] == '' ? [] : explode(',', $result[28]);
+    // convert keynote
+    $result[38] = $result[38] === '是';
+    // tags transform
+    $tags = $result[28];
+    $tagItem = [];
+    foreach ($tags as $tagKey => $tag) {
+        // $tagItem[$tagKey] = $tagTrans[$tag];
+        $tagItem[] = [
+            'color' => [
+                'web' => "#651fff",
+                'mobile' => "#ffcc00"
+            ],
+            'name' => $tagTrans[$tag]
+        ];
+    }
     $newData = [
         'name' => $result[1],
         'name_e' => $result[2],
@@ -61,29 +104,30 @@ while (($row = fgets($f)) !== false) {
         'company_e' => $result[5],
         'job_title' => $result[6],
         'job_title_e' => $result[7],
-        'bio' => $result[10],
-        'bio_e' => $result[11],
-        'photo_for_speaker_web' => 'api/2020/speaker/images/web/speaker_' . (string) $speaker_id,
-        'photo_for_speaker_mobile' => 'api/2020/speaker/images/mobile/speaker_' . (string) $speaker_id,
-        'photo_for_session_web' => 'api/2020/speaker/images/web/speaker_' . (string) $speaker_id,
-        'photo_for_session_mobile' => 'api/2020/speaker/images/mobile/speaker_' . (string) $speaker_id,
-        'photo_for_sponsor_web' => 'api/2020/speaker/images/web/speaker_' . (string) $speaker_id,
-        'photo_for_sponsor_mobile' => 'api/2020/speaker/images/mobile/speaker_' . (string) $speaker_id,
-        'link_fb' => $result[13],
-        'link_github' => $result[14],
-        'link_twitter' => $result[15],
-        'link_other' => $result[16],
-        'link_slide' => '', //$result[17],
-        'topic' => $result[19],
-        'topic_e' => $result[20],
-        'summary' => $result[21],
-        'summary_e' => $result[22],
+        'bio' => $result[11],
+        'bio_e' => $result[12],
+        'img' => [
+            'web' => 'api/2021/speaker/images/web/speaker_' . (string) $speaker_id,
+            'mobile' => 'api/2021/speaker/images/mobile/speaker_' . (string) $speaker_id
+        ],
+        'link_fb' => $result[14],
+        'link_github' => $result[15],
+        'link_twitter' => $result[16],
+        'link_other' => $result[17],
+        'link_slide' => '', //$result[18],
+        'topic' => $result[24],
+        'topic_e' => $result[25],
+        'summary' => $result[26],
+        'summary_e' => $result[27],
         "community_partner" => "",
-        'is_keynote' => $result[37],
-        "is_online" => false,
-        'recordable' => $result[25] == '否' ? false : true,
-        'level' => explode('-', $result[24])[0],
-        'tags' => $result[23],
+        'is_keynote' => $result[38],
+        "is_online" => true,
+        'recordable' => true, //$result[33] == '否' ? false : true,
+        'level' => explode('-', $result[29])[0],
+        'tags' => $tagItem,
+        'target' => $result[30], // 目標會眾
+        'prior_knowledge' => $result[31], // 先備知識
+        'expected_gain' => $result[32] // 預期收穫
     ];
 
     if (array_key_exists($newData['speaker_id'], $data)) {
